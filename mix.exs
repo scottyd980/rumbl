f3d91a7d7cfa6a97e234c22052e94d1616a052b2
Douglass, Scott <scott.douglass@fmr.com>
2016-09-29 21:04:22 -0400

    working through otp and chapter 11

--- mix.exs
+++ mix.exs
@@ -38,7 +38,8 @@ defmodule Rumbl.Mixfile do
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
      {:cowboy, "~> 1.0"},
-     {:comeonin, "~> 2.5"}]
+     {:comeonin, "~> 2.5"},
+     {:sweet_xml, "~> 0.5.0"}]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
